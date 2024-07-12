4df167a6d412dc55726be8d0094a34100802dac1
nilakr@icloud.com <nila.krishnasami@gmail.com>
2024-07-12 11:58:44 -0700

    Initial Commit

---
+++ GitHubPractice/GitHubPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubPracticeApp.swift
+//  GitHubPractice
+//
+//  Created by Nila Krishnasami on 7/12/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHubPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
